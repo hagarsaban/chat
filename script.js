978a14df3a6398a837e38dbb419b2ab04d139cc0
CrumpyOldLord <wessel32@gmail.com>
2013-12-20 18:01:08 +0100

    add notifications

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function show_grid() {
     class_list.remove('hidden');
 }
 function hide_grid() {
+    window.removeEventListener("scroll", hide_grid, false);
     class_list.add('hidden');
 }
 function toggle_grid() {
@@ -14,3 +15,37 @@ function toggle_grid() {
     }
 };
 
+
+
+
+const count_div = document.getElementById('notification-count')
+function notification() {
+    if(confirm("You have " + 
+               notification.count + 
+               " notifications\n" + 
+               "Shall I add one?")) {
+        notification.count += 1;
+    } else {
+        if (notification.count) {
+            notification.count -= 1
+        }
+    }
+    notification.update();
+}
+notification.count = parseInt(count_div.textContent);
+notification.update = function update() {
+    const previous = parseInt(count_div.textContent);
+    const next     = notification.count;
+    
+    if (next === previous) {
+        return;
+    } else {
+        if (next === 0) {
+            count_div.style.display = "none"
+            count_div.textContent = 0;
+        } else {
+            count_div.style.display = "inline"
+            count_div.textContent = next;
+        }
+    }
+}
